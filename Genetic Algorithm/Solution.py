6218377c39f17b57918601802ba6ab7036cd38f6
guillaume gouhier <guillaume.gouhier1999@gmail.com>
2021-10-09 17:02:20 +0200

    Begin Genetic; Begin Solution Class; Random Genes done.

---
+++ Genetic Algorithm/Solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    """docstring for Solution."""
+
+    def __init__(self, arg):
+        super(Solution, self).__init__()
+        self.arg = arg
+
+## TODO: Check If Solution Is Viable
+    def isPossible(self):
+        return True
---
+++ Genetic Algorithm/functions.py
@@ -0,0 +1,4 @@
+import random
+
+def generateGenes():
+    return [random.randint(0,1) for i in range(300)]
