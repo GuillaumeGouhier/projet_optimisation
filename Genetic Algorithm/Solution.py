52f6bad0df2c11487a33c57d468c8a553e6fc573
guillaume gouhier <guillaume.gouhier1999@gmail.com>
2021-10-10 06:50:54 +0200

    Begin repair. Not tested yet

--- Genetic Algorithm/Solution.py
+++ Genetic Algorithm/Solution.py
@@ -11,3 +11,14 @@ class Solution(object):
 
     def mutate(self, proba):
         self.genes = list(map(lambda x : 1-x if random.random() <= proba else x, self.genes ))
+        return 0
+
+    def repair (self, data):
+        # Repair phase to ensure every child solution can live
+        invites_candidate = [i for i in range(len(self.genes)) if self.genes[i]==1 ]
+        # Map invite to score
+        scores = list(map(lambda x: (len([j for j in invites_candidate if j not in data[x].getListeConnu()]), x), invites_candidate).sort(reverse=True)
+        # Remove if too problematic
+        while self.isPossible():
+            self.genes[invites_candidate[scores[i][1]]] = 0
+        # Till everyone knows everyone
